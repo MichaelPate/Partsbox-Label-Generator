47734fde9283e87185b97ffcefaa4eb9d3cde6cc
Michael Pate <pate5800@gmail.com>
2025-03-14 23:00:56 -0600

    find ID by part number implemented

--- LabelGenerator/partsbox_api.py
+++ LabelGenerator/partsbox_api.py
@@ -7,6 +7,7 @@ with open("secrets.txt", 'r') as file:
         api_key = first_line.split(",")[1]
 
 part_id = "fj7h5erw0rj499evktbvhetc45"
+part_num = "CGA0402C0G220J500GT"
 
 '''
 # Define the API endpoint and headers
@@ -65,15 +66,17 @@ def process_response(response_data):
 def find_part_id_by_number(part_number, api_key):
     # First check the local file to see if we have used this part number before
     # File format is 'partNumber partID'
-    with open('partsID.txt', 'r') as partsFile:
+    with open('partsID.txt') as partsFile:
+        set_ids = set([])
         for line in partsFile:
-            partNum = line.split(" ")[0]
-            partID = line.split(" ")[1]
+            partNum = line.split(" ")[0].strip()
+            partID = line.split(" ")[1].strip()
+            set_ids.add(partID)
 
             if partNum == part_number:
                 return partID
-
-    # If we got to this point then the part ID does not exist in the file, so we gotta talk to the API
+    
+    # If we got to this point then the part ID does not exist in the file, so we now gotta talk to the API
 
     # Use API to get all the parts in the database
     url = "https://api.partsbox.com/api/1/part/all"
@@ -81,19 +84,28 @@ def find_part_id_by_number(part_number, api_key):
         "Content-Type": "application/json",
         "Authorization": "APIKey " + api_key
     }
-
     response = requests.post(url, headers=headers)
-
     if response.status_code != 200:
         print(f"Request failed with status code {response.status_code}")
         print("Response:", response.text)
         return -1
-
-    # Now we add all the part IDs to our file that dont already exist
     parts_dict = process_response(response.json())
+    
+    # Since we polled the API lets reconcile with the local file to prevent the API call again until new parts are added
+    with open('partsID.txt', 'a') as partsFile:
+        for key in parts_dict:
+            if key not in set_ids:
+                partsFile.write(f"{parts_dict[key]} {key}\n")
+                print(f"Added ID {key} for part number {parts_dict[key]} to local file.")
+        
+    # Now parts_dict has the ID
+    for key in parts_dict:
+        if parts_dict[key] == part_number:
+            return key
+    
+    # We got all the way here and did not find it so there was an error
+    return -1
 
-    # Print the resulting dictionary
-    print(parts_dict)
 
 if __name__ == '__main__':
-    find_part_id_by_number(part_id, api_key)
+    print(find_part_id_by_number(part_num, api_key))
